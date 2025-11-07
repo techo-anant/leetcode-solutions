8c6290889391c98f43035b5ec3e289da875a4307
Anant Kumar Singh <anantk.singh9876@gmail.com>
2025-11-06 22:23:47 -0500

    Added solution to string to integer question

--- Easy/same-tree.ts
+++ Easy/same-tree.ts
@@ -50,4 +50,4 @@ function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
     if (p.val === q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
     return false;
-};
+}
