7e99080466405e1f4d4b6bff6f4b997297912929
Anant Kumar Singh <anantk.singh9876@gmail.com>
2025-11-21 16:37:07 -0500

    Added 2 solutions for eazy hashtable - linked list cycle question

---
+++ Easy/hash-tables/linked-list-cycle.py
@@ -0,0 +1,75 @@
+## Question
+
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [3,2,0,-4], pos = 1
+# Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+# Example 2:
+
+
+# Input: head = [1,2], pos = 0
+# Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+# Example 3:
+
+
+# Input: head = [1], pos = -1
+# Output: false
+# Explanation: There is no cycle in the linked list.
+ 
+
+# Constraints:
+
+# The number of the nodes in the list is in the range [0, 104].
+# -105 <= Node.val <= 105
+# pos is -1 or a valid index in the linked-list.
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Answer Time = O(n), Space = O(n)
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        reached = set()
+
+        curr = head
+        while curr:
+            if curr in reached:
+                return True
+            else:
+                reached.add(curr)
+                curr = curr.next
+        
+        return False
+    
+
+# Answer Time = O(n), Space = O(1)
+# Using Floyd's Turtle ans Hare concept
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        hare = turtle = head
+
+        while hare and hare.next:
+            turtle = turtle.next
+            hare = hare.next.next
+
+            if turtle == hare:
+                return True
+        
+        return False  
