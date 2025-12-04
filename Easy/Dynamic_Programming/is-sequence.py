f94e8dd3cce51f862a10e1a087966efbb0715965
Anant Kumar Singh <anantk.singh9876@gmail.com>
2025-12-03 23:07:50 -0500

    Added Optimized soln to dp eazy: is sequence

---
+++ Easy/Dynamic_Programming/is-sequence.py
@@ -0,0 +1,49 @@
+# Question
+
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 100
+# 0 <= t.length <= 104
+# s and t consist only of lowercase English letters.
+ 
+
+# Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+# Solution Time: O(m*n), Space: O(n*m)
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # optimal solution can be reached using two pointers approach
+
+        m, n = len(s), len(t)
+
+        dp = [[False]*(n+1) for _ in range(m+1)]
+
+        for i in range(n+1):
+            dp[0][i] = True
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = dp[i][j-1]
+        
+
+        return dp[m][n]
