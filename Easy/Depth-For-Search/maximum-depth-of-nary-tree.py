9c47cb7a18f23801d2353877ddaf3936d56bc774
Anant Kumar Singh <anantk.singh9876@gmail.com>
2025-12-18 17:15:04 -0500

    Soln for eazy dfs - maximum depth of n-ary tree

---
+++ Easy/Depth-For-Search/maximum-depth-of-nary-tree.py
@@ -0,0 +1,52 @@
+# Given a n-ary tree, find its maximum depth.
+
+# The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+# Example 1:
+
+
+
+# Input: root = [1,null,3,2,4,null,5,6]
+# Output: 3
+# Example 2:
+
+
+
+# Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+# Output: 5
+ 
+
+# Constraints:
+
+# The total number of nodes is in the range [0, 104].
+# The depth of the n-ary tree is less than or equal to 1000.
+
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+
+# Answer: Time: O(n), Space: O(n)
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+
+        def dfs( node , curr):
+            if not node:
+                return 0
+            if not node.children:
+                return curr
+            
+            maxx = 0
+            for child in node.children:
+                dep = dfs(child, curr+1)
+                if maxx < dep:
+                    maxx = dep
+            
+            return maxx
+                    
+        return dfs(root, 1)
