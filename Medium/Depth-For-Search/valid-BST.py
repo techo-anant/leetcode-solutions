8f907379435bba21a030c732eeecaf3dfc037722
Anant Kumar Singh <anantk.singh9876@gmail.com>
2025-12-19 18:14:03 -0500

    Soln to med dfs - Valid BST

---
+++ Medium/Depth-For-Search/valid-BST.py
@@ -0,0 +1,61 @@
+# Question 
+
+# Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+# A valid BST is defined as follows:
+
+# The left subtree of a node contains only nodes with keys strictly less than the node's key.
+# The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+ 
+
+# Example 1:
+
+
+# Input: root = [2,1,3]
+# Output: true
+# Example 2:
+
+
+# Input: root = [5,1,4,null,null,3,6]
+# Output: false
+# Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 104].
+# -231 <= Node.val <= 231 - 1
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Answer:- Time: O(n), Space: O(n)
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def isBST ( node , inorder):
+            if not node:
+                return True
+            
+            if node.left:
+                if not isBST(node.left, inorder):
+                    return False
+            
+            if inorder:
+                if node.val <= inorder[-1]:
+                    return False
+            inorder.append(node.val)
+
+            if node.right:
+                if not isBST(node.right, inorder):
+                    return False
+                
+            return True
+
+        return isBST( root, [])
